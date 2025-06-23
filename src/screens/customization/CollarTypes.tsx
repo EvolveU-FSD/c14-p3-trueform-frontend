fab3c0d6d3081bef4cedbdbbd5372552b6c2ac09
vathsalakh <vathsala.kh@gmail.com>
2025-06-22 18:02:47 -0600

    style: Add new collar images and update image references in CollarTypes.tsx

--- src/screens/customization/CollarTypes.tsx
+++ src/screens/customization/CollarTypes.tsx
@@ -6,63 +6,28 @@ const COLLAR_OPTIONS = [
   {
     id: 'italian1',
     title: 'Italian Collar 1 Button',
-    image: { uri: 'https://www.propercloth.com/collars/italian1.jpg' },
+    image: require('../../../assets/images/collar/Italian Collar 1 Button.jpeg'),
   },
-  {
-    id: 'italian2',
-    title: 'Italian Collar 2 Button',
-    image: { uri: 'https://www.propercloth.com/collars/italian2.jpg' },
-  },
+ 
+  
   {
     id: 'french1',
     title: 'French Collar 1 Button',
-    image: { uri: 'https://www.propercloth.com/collars/french1.jpg' },
-  },
-  {
-    id: 'french2',
-    title: 'French Collar 2 Button',
-    image: { uri: 'https://www.propercloth.com/collars/french2.jpg' },
+    image: require('../../../assets/images/collar/French Collar 1 Button.jpeg'),
   },
   {
     id: 'cutaway1',
     title: 'Cut Away 1 Button',
-    image: { uri: 'https://www.propercloth.com/collars/cutaway1.jpg' },
+    image: require('../../../assets/images/collar/Cut Away 1 Button.jpeg'),
   },
-  {
-    id: 'cutaway2',
-    title: 'Cut Away 2 Button',
-    image: { uri: 'https://www.propercloth.com/collars/cutaway2.jpg' },
-  },
-  {
-    id: 'round',
-    title: 'Round Collar',
-    image: { uri: 'https://www.propercloth.com/collars/round.jpg' },
-  },
+  
+ 
   {
     id: 'buttondown',
     title: 'Button Down',
-    image: { uri: 'https://www.propercloth.com/collars/buttondown.jpg' },
+     image: require('../../../assets/images/collar/Button Down.jpeg'),
   },
-  {
-    id: 'tab',
-    title: 'Tab Collar',
-    image: { uri: 'https://www.propercloth.com/collars/tab.jpg' },
-  },
-  {
-    id: 'hidden',
-    title: 'Hidden Button',
-    image: { uri: 'https://www.propercloth.com/collars/hidden.jpg' },
-  },
-  {
-    id: 'batman',
-    title: 'Batman Collar',
-    image: { uri: 'https://www.propercloth.com/collars/batman.jpg' },
-  },
-  {
-    id: 'modern',
-    title: 'Modern Collar',
-    image: { uri: 'https://www.propercloth.com/collars/modern.jpg' },
-  },
+  
 ];
 
 export default function CollarStyle() {
